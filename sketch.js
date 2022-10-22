9cf58efdf5420d82a3de44ce91a0df170f263c17
Joe P Shoulak <joe@squarecompass.io>
2022-10-22 13:35:44 -0500

    perlin 2d noise implemented

--- sketch.js
+++ sketch.js
@@ -1,11 +1,30 @@
 function setup() {
   createCanvas(200, 200);
-  background(255);
+  pixelDensity(1);
 }
 
+const increment = 0.01;
+// const scale = 20;
+
 function draw() {
-  stroke(255);
-  if (mouseIsPressed === true) {
-    line(mouseX, mouseY, pmouseX, pmouseY);
+  loadPixels();
+
+  let yOff = 0;
+  for (let y = 0; y < width; y++) {
+    let xOff = 0;
+    for (let x = 0; x < width; x++) {
+      let index = (x + y * width) * 4;
+      const r = noise(xOff, yOff) * 255;
+      pixels[index + 0] = r;
+      pixels[index + 1] = r;
+      pixels[index + 2] = r;
+      pixels[index + 3] = 255;
+
+      xOff += increment;
+    }
+    yOff += increment;
   }
+
+  updatePixels();
+  noLoop();
 }
